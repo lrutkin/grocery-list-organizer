ade25bdc2b2c2b845a851e79d0ed354f5305054a
Leslie Rutkin <lrutkin@gmail.com>
2022-04-10 15:55:03 -0700

    add li functionality to js and hide form
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>
    Co-authored-by: Joel Lake <joelcooperlake@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   $("#glo").submit(function(event) {
+    $("#glo").hide();
     const vegetable = $("#vegetable").val().toUpperCase();
     const fruit = $("#fruit").val().toUpperCase();
     const dairy = $("#dairy").val().toUpperCase();
@@ -8,8 +9,15 @@ $(document).ready(function() {
     let groceryList = [vegetable, fruit, dairy, protein, dessert];
     
     groceryList.sort(); 
-
     
+    groceryList.forEach(function(item) {
+      let list = $("#finList");
+      let li = document.createElement("li");
+      li.innerText = item;
+      list.append(li);
+    //const sortArray = groceryList.slice();
+    //sortArray.toString();
+    });
 
     event.preventDefault();
   }); 
