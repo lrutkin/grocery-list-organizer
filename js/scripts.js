f7c46765bb56050f6880c9d2569e19aa3aae6e1e
Leslie Rutkin <lrutkin@gmail.com>
2022-04-10 14:55:19 -0700

    add initial js to make and sort array
    Co-authored-by: Niles O'Neil <nilesoneil@gmail.com>
    Co-authored-by: Joel Lake <joelcooperlake@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-$(document).ready(function)
+$(document).ready(function() {
+  $("#glo").submit(function(event) {
+    const vegetable = $("#vegetable").val().toUpperCase();
+    const fruit = $("#fruit").val().toUpperCase();
+    const dairy = $("#dairy").val().toUpperCase();
+    const protein = $("#protein").val().toUpperCase();
+    const dessert = $("#dessert").val().toUpperCase();
+    let groceryList = [vegetable, fruit, dairy, protein, dessert];
+    
+    groceryList.sort(); 
+
+    
+
+    event.preventDefault();
+  }); 
+});
